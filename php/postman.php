38bdaad880d0b57613ca19f6e8907f54a6fa75f0
Codeunit6 <fernando.brayan.m.g@gmail.com>
2021-11-11 16:32:49 -0600

    PHPMailer con Heroku

--- php/postman.php
+++ php/postman.php
@@ -42,9 +42,9 @@ try {
     $mail->isSMTP();                                            //Send using SMTP
     $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-    $mail->Username = getenv('SMTP_USERNAME');
+    $mail->Username = 'fernando.brayan.m.g@gmail.com';
     //Assuming SMTP_PASSWORD is your environment variable which holds password
-    $mail->Password = getenv('SMTP_PASSWORD');                             //SMTP password
+    $mail->Password = 'holafernando12';                             //SMTP password
     $mail->SMTPSecure = 'tls';            //Enable implicit TLS encryption
     $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
